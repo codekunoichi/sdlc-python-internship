20443398ed87b215dff733fa100c25493e4a9361
Rumpa Giri <codekunoichi@users.noreply.github.com>
2023-06-16 15:56:36 +0000

    add legible docstring

--- Chap_6_SaaSDev/hello_world.py
+++ Chap_6_SaaSDev/hello_world.py
@@ -9,12 +9,23 @@ app = FastAPI()
 def root() -> None:
     """
         This is the main function that will be called when the application is run
+        call using localhost:8000
     """
     return "Welcome to the sample fastAPI App!"
 
 @app.get("/hello")
 def hello_world() -> None:
     """
-        This is the main function that will be called when the application is run
+        returns Hello World!
+        call using localhost:8000/hello
     """
     return "Hello World!"
+
+@app.get("/hello/{name}")
+def hello_name(name: str) -> None:
+    """
+        returns the name passed in the URL 
+        call using localhost:8000/hello/<name>
+    """
+    return f"Hello {name}!"
+
